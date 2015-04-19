e52b3c191b7fcb4749c9ba371fba3fed613d7255
Kevin Tran <kevintran@ricoh.sg>
2015-04-19 22:32:01 +0800

    redirect to homepage and show user

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,10 +1,9 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def facebook
-    #render json: { login: :success, auth: request.env["omniauth.auth"] }
     user = User.find_by_facebook_omniauth(omniauth_params)
 
-    # render json: { login: :success, auth: request.env["omniauth.auth"] }
     sign_in user
+    redirect_to root_path
   end
 
   private
