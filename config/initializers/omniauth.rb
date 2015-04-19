a6506d147c64d4789d993578391e7df2b4b67838
Kevin Tran <kevintran@ricoh.sg>
2015-04-19 23:16:27 +0800

    add publish_action to omniauth initializer

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -3,7 +3,7 @@ Rails.application.config.middleware.use OmniAuth::Builder do
     :facebook,
     ENV['FB_APP_ID'],
     ENV['FB_APP_SECRET'],
-    scope: 'email,read_stream',
+    scope: 'email,read_stream,publish_actions',
     prompt: "select_account",
     display: "popup"
   )
